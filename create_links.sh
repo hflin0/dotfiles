11e01e5939700b84d12e4b4858d717413fc63c0a
Hafid Lin <hafid.lin@nsn.com>
2013-04-10 18:07:34 +0800

    update create_links.sh

--- create_links.sh
+++ create_links.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-ln .exrc ~/
-ln -s .vim ~/
-ln -s .git ~/
-ln -s .gitconfig ~/
-ln .tmux.conf ~/
+ln_file()
+{
+  src=`pwd`/$1
+  file_name=`basename $1`
+  dst_dir=$2
+  dst_file=${dst_dir}/${file_name}
+  if [ -s ${dst_file} ];then
+    rm -vf ${dst_file}
+  fi
+  ln -s ${src} ${dst_file}
+}
+
+ln_file .exrc ~/
+ln_file .vim ~/
+ln_file .git ~/
+ln_file .gitconfig ~/
+ln_file .tmux.conf ~/
